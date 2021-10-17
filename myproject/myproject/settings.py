523f41fddb46c529801aeaeaa95e689fe1e42d64
aminsadeghi1990z <aminsadeghi1990z@gmail.com>
2021-10-17 23:19:02 +0330

    4

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -126,6 +126,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
